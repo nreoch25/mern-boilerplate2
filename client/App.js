f8161f1191749c1b3a3a58a5501df4a36793b872
Nigel Reoch <nreoch9@gmail.com>
2018-07-20 09:29:21 -0400

    Added Register component

--- client/App.js
+++ client/App.js
@@ -2,11 +2,16 @@ import React, { Component } from "react";
 import { Route } from "react-router-dom";
 // Components
 import Index from "./components/pages/Index";
+import Register from "./components/auth/Register";
+
 class App extends Component {
   render() {
     return (
-      <div className="container">
+      <div className="App">
         <Route exact path="/" component={Index} />
+        <div className="container">
+          <Route exact path="/register" component={Register} />
+        </div>
       </div>
     );
   }
