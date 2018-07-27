132d64629e1e8da86ab8947c6deb949e08d27047
Nigel Reoch <nreoch9@gmail.com>
2018-07-27 15:56:43 -0400

    Added Dashboard Profile

--- client/components/pages/Dashboard.js
+++ client/components/pages/Dashboard.js
@@ -3,19 +3,37 @@ import { connect } from "react-redux";
 import { getCurrentUser } from "../../actions/authActions";
 
 class Dashboard extends Component {
-  componentDidMount() {
-    this.props.getCurrentUser();
-  }
   render() {
+    console.log("AUTH", this.props.auth);
     return (
-      <div>
-        <h1>Dashboard</h1>
+      <div className="profiles">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <div className="card card-body bg-light mb-3">
+                <div className="row">
+                  <div className="col-2">
+                    <img
+                      src={this.props.auth.user.avatar}
+                      alt=""
+                      className="rounded-circle"
+                    />
+                  </div>
+                  <div className="col-lg-6 col-md-4 col-8">
+                    <h3>{this.props.auth.user.name}</h3>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
 }
 
-export default connect(
-  null,
-  { getCurrentUser }
-)(Dashboard);
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(mapStateToProps)(Dashboard);
