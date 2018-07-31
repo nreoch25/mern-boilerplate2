935bb8f83918f79b790cc4611d68e735aba1da20
Nigel Reoch <nreoch9@gmail.com>
2018-07-31 16:46:37 -0400

    Added Profile display

---
+++ client/components/profile/ProfileHeader.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import isEmpty from "../../utils/isEmpty";
+
+class ProfileHeader extends Component {
+  render() {
+    const { profile } = this.props;
+
+    return (
+      <div className="row">
+        <div className="col-md-12">
+          <div className="card card-body bg-info text-white mb-3">
+            <div className="row">
+              <div className="col-4 col-md-3 m-auto">
+                <img
+                  className="rounded-circle"
+                  src={profile.user.avatar}
+                  alt=""
+                />
+              </div>
+            </div>
+            <div className="text-center">
+              <h1 className="display-4 text-center">{profile.user.name}</h1>
+              {isEmpty(profile.location) ? null : <p>{profile.location}</p>}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ProfileHeader;
